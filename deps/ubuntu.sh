0369706a8f88348fec234412015318bd5f67b253
Shane Scott <sscott@gvit.com>
2018-10-15 09:37:21 -0500

    Dep bug fix, Add pull based reinit

--- deps/ubuntu.sh
+++ deps/ubuntu.sh
@@ -2,8 +2,8 @@
 echo "Updating Apt database..."
 apt-get -qq update
 echo "Installing python dependancies..."
-apt-get -qq install python3-pyqt5 python3-pyqt4 python3-pyside.qtwebkit python3-sqlalchemy* python3-pip3 -y
+apt-get -qq install python3-pyqt5 python3-pyqt4 python3-pyside.qtwebkit python3-sqlalchemy* python3-pip -y
 echo "Installing external binaryies and application dependancies..."
 apt-get -qq install finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb gksu -y
-echo "Installing Pythin Libraries..."
+echo "Installing Python Libraries..."
 pip3 install asyncio aiohttp aioredis aiomonitor apscheduler Quamash
