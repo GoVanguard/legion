61f90833ec03ce25466c1d1a16a87185224bd730
Shane Scott <sscott@gvit.com>
2019-01-11 12:26:35 -0600

    CI changes

--- deps/installPythonLibs.sh
+++ deps/installPythonLibs.sh
@@ -2,11 +2,12 @@
 # Setup Python deps
 
 testForPip=`pip --version`
+
 if [[ $testForPip == *"3.6"* ]]; then
     pipBin='pip'
 else
     pipBin='pip3.6'
 fi
 
-sudo $pipBin install sqlalchemy pyqt5 asyncio aiohttp aioredis aiomonitor apscheduler Quamash
-sudo $pipBin install service_identity --upgrade
+#sudo $pipBin install sqlalchemy pyqt5 asyncio aiohttp aioredis aiomonitor apscheduler Quamash
+#sudo $pipBin install service_identity --upgrade
