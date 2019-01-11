287f6ea9ab860b8f17f7d1fcf9aacd7b7ea601d9
Shane S <gs@shanewilliamscott.com>
2019-01-11 12:40:52 -0600

    CI changes

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 try:
     from sqlalchemy.orm.scoping import ScopedSession as scoped_session
+    print("SQL Alchemy library OK")
 except ImportError as e:
     print("Import failed. SQL Alchemy library not found.")
     exit(1)
 
 try:
     from PyQt5 import QtWidgets, QtGui, QtCore
+    print("PyQt5 library OK.")
 except ImportError as e:
     print("Import failed. PyQt5 library not found.")
     exit(1)
@@ -13,6 +15,7 @@ except ImportError as e:
 try:
     import quamash
     import asyncio
+    print("Quamash and asyncio libraries OK.")
 except ImportError as e:
     print("Import failed. quamash or asyncio not found.")
     exit(1)
@@ -22,6 +25,7 @@ try:
     from ui.gui import *
     from ui.view import *
     from controller.controller import *
+    print("Legion class imports OK.")
 except ImportError as e:
     print("Import failed. One or more modules failed to import correctly.")
     exit(1)
