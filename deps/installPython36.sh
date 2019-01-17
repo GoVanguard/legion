55203147b939cc999929ed615ce46d0499f5bb8c
Shane Scott <sscott@gvit.com>
2019-01-17 15:10:41 -0600

    Dep installers updated to detect if apt update needed

--- deps/installPython36.sh
+++ deps/installPython36.sh
@@ -2,7 +2,7 @@
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
 then
     echo "Installing python3.6 from APT..."
     echo "Updating Apt database..."
@@ -11,27 +11,35 @@ then
     sudo apt-get install -yqq python3 python3-pip python-netlib 2>&1 > /dev/null
 else
     echo "Python3.6 found!"
+    echo "Python 3.6: ${PYTHON3BIN}"
+    echo "PIP 3.6: ${PIP3BIN}"
     exit 0
 fi
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
 then
     echo "Installing python3.6 from source..."
     sudo ./buildPython36.sh
 else
     echo "Python3.6 found!"
+    echo "Python 3.6: ${PYTHON3BIN}"
+    echo "PIP 3.6: ${PIP3BIN}"
     exit 0
 fi
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
 then
     echo "Everything went wrong trying to get python3.6 setup. Please do this manually."
+    echo "Python 3.6: ${PYTHON3BIN}"
+    echo "PIP 3.6: ${PIP3BIN}"
     exit 1
 else
     echo "Python3.6 found!"
+    echo "Python 3.6: ${PYTHON3BIN}"
+    echo "PIP 3.6: ${PIP3BIN}"
     exit 0
 fi
