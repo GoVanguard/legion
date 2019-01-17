c7a873f535ed6426f7d28a06fe395b019f073efa
sscottgvit <sscott@govanguard.com>
2019-01-17 15:19:45 -0600

    Dep installers updated to detect if apt update needed

--- deps/installPython36.sh
+++ deps/installPython36.sh
@@ -2,7 +2,7 @@
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" ]] | [[ ${PIP3BIN} == "Missing" ]]
 then
     echo "Installing python3.6 from APT..."
     echo "Updating Apt database..."
@@ -18,7 +18,7 @@ fi
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" ]] | [[ ${PIP3BIN} == "Missing" ]]
 then
     echo "Installing python3.6 from source..."
     sudo ./buildPython36.sh
@@ -31,7 +31,7 @@ fi
 
 source ./detectPython.sh
 
-if [[ ${PYTHON3BIN} == "Missing" | ${PIP3BIN} == "Missing" ]]
+if [[ ${PYTHON3BIN} == "Missing" ]] | [[ ${PIP3BIN} == "Missing" ]]
 then
     echo "Everything went wrong trying to get python3.6 setup. Please do this manually."
     echo "Python 3.6: ${PYTHON3BIN}"
