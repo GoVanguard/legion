b06b28996d693489e3f8fae9a55f85cb66df0e86
Shane Scott <sscott@gvit.com>
2019-01-18 04:31:02 -0600

    Updated dep installers

--- deps/Ubuntu-16WSL.sh
+++ deps/Ubuntu-16WSL.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-source ./deps/detectPython.sh
-
-#cp ./deps/*.sh /tmp
-#cp ./requirements.txt /tmp -f
-#cd /tmp
 chmod a+x ./deps/*.sh
 
 ./deps/installDeps.sh
 ./deps/installPython36.sh
+
+source ./deps/detectPython.sh
 
 echo "Installing external binaryies and application dependancies..."
 apt-get -yqqq install finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb -y
