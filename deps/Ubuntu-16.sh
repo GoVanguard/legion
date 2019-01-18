82634ddd5af40344b01075b8678aed3a6e7291bc
sscottgvit <sscott@govanguard.com>
2019-01-18 05:05:24 -0600

    Dep installer updates

--- deps/Ubuntu-16.sh
+++ deps/Ubuntu-16.sh
@@ -7,8 +7,5 @@ chmod a+x ./deps/*.sh
 
 source ./deps/detectPython.sh
 
-echo "Installing external binaryies and application dependancies..."
-apt-get -yqqq install finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb -y
-
 echo "Installing Python Libraries..."
 ./deps/installPythonLibs.sh
--- deps/installPythonLibs.sh
+++ deps/installPythonLibs.sh
@@ -4,5 +4,4 @@ source ./deps/detectPython.sh
 
 # Setup Python deps
 ${PIP3BIN} install -r requirements.txt
-${PIP3BIN} install sqlalchemy pyqt5 asyncio aiohttp aioredis aiomonitor apscheduler Quamash
 ${PIP3BIN} install service_identity --upgrade
