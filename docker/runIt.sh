4408eb8323a43417d54cffa7ab6ae922b0c090cc
Shane Scott <sscott@gvit.com>
2019-01-23 12:23:51 -0600

    Remove settings menu; Remove resize of columns on refresh

--- docker/runIt.sh
+++ docker/runIt.sh
@@ -2,4 +2,4 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH testing
+docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH legion
