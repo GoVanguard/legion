2e39cf65bc182dd2438be0aaa9ad7987eb09c458
Shane S <gs@shanewilliamscott.com>
2019-02-22 11:48:57 -0600

    Add backup folder, disable save every close even if there are no changes

--- controller/controller.py
+++ controller/controller.py
@@ -28,7 +28,7 @@ class Controller():
     def __init__(self, view, logic):
         self.name = "LEGION"
         self.version = '0.3.0'
-        self.build = '1550857610'
+        self.build = '1550857694'
         self.author = 'GoVanguard'
         self.copyright = '2019'
         self.emails = ['hello@gvit.com']
