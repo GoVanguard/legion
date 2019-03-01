5496c70fee31ee277ee6ac43b0890e02055d5710
jchoy14 <28782996+jchoy14@users.noreply.github.com>
2019-03-01 14:50:18 -0500

    Added additional deps and attempted to fix OS accuracy issue

--- parsers/OS.py
+++ parsers/OS.py
@@ -22,7 +22,7 @@ class OS:
             self.generation = OSNode.getAttribute('osgen')
             self.os_type = OSNode.getAttribute('type')
             self.vendor = OSNode.getAttribute('vendor')
-            self.accuracy = OSNode.getAttribute('accuracy')
+            self.accuracy = int(OSNode.getAttribute('accuracy'))
 
 if __name__ == '__main__':
 
