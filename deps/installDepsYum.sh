e07b8b859d4ae6aba299715203302b83224b2bda
jchoy14 <28782996+jchoy14@users.noreply.github.com>
2019-03-04 14:52:52 -0500

    Missed one

--- deps/installDepsYum.sh
+++ deps/installDepsYum.sh
@@ -7,4 +7,4 @@ echo "Checking Yum..."
 runYumGetUpdate
 
 echo "Installing deps..."
-yum install -y epel-release centos-release-scl python36-pip python36-devel nmap finger hydra nikto rpcbind sra-toolkit sslscan rwho medusa CutyCapt eog hping3 python-sqlalchemy.x86_64 PyQt4 rh-python36-python-sqlalchemy.x86_64 python-impacket ruby perl
+yum install -y epel-release centos-release-scl python36-pip python36-devel nmap finger hydra nikto rpcbind sslscan rwho medusa CutyCapt eog hping3 python-sqlalchemy.x86_64 PyQt4 rh-python36-python-sqlalchemy.x86_64 python-impacket ruby perl
