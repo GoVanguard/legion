e7dc20b10cbeb9c5a7bb3e7be1bac28348ea88b7
jchoy14 <28782996+jchoy14@users.noreply.github.com>
2019-03-04 14:46:51 -0500

    Removed missing deps

--- deps/installDepsYum.sh
+++ deps/installDepsYum.sh
@@ -7,4 +7,4 @@ echo "Checking Yum..."
 runYumGetUpdate
 
 echo "Installing deps..."
-yum install -y epel-release centos-release-scl python36-pip python36-devel nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 python-sqlalchemy.x86_64 PyQt4 rh-python36-python-sqlalchemy.x86_64 python-impacket ruby perl
+yum install -y epel-release centos-release-scl python36-pip python36-devel nmap finger hydra nikto rpcbind sra-toolkit sslscan rwho medusa CutyCapt eog hping3 python-sqlalchemy.x86_64 PyQt4 rh-python36-python-sqlalchemy.x86_64 python-impacket ruby perl
