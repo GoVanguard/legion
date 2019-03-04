357da50533cb146b6a056d13d78c107b95f41b2b
jchoy14 <28782996+jchoy14@users.noreply.github.com>
2019-03-04 14:04:45 -0500

    Updated installdeps script

--- deps/installDepsYum.sh
+++ deps/installDepsYum.sh
@@ -6,4 +6,5 @@ source ./deps/yum.sh
 echo "Checking Yum..."
 runYumGetUpdate
 
-echo "Please install the following deps: centos-release-scl nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 python-impacket ruby perl"
+echo "Installing deps..."
+yum install -y epel-release centos-release-scl python36-pip python36-devel nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 python-sqlalchemy.x86_64 PyQt4 rh-python36-python-sqlalchemy.x86_64 python-impacket ruby perl
