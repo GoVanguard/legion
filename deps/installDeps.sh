5384a1929242b1f69940431f74df74ea56e686a2
Shane S <gs@shanewilliamscott.com>
2019-03-06 15:31:45 -0600

    Add python3.7 support back, Silence some logging, Add some deps

--- deps/installDeps.sh
+++ deps/installDeps.sh
@@ -7,4 +7,4 @@ echo "Checking Apt..."
 runAptGetUpdate
 
 echo "Installing deps..."
-DEBIAN_FRONTEND="noninteractive" apt-get -yqqq --allow-unauthenticated --force-yes -o DPkg::Options::="--force-overwrite" -o DPkg::Options::="--force-confdef" install nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 sqlmap wapiti
+DEBIAN_FRONTEND="noninteractive" apt-get -yqqq --allow-unauthenticated --force-yes -o DPkg::Options::="--force-overwrite" -o DPkg::Options::="--force-confdef" install nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 sqlmap wapiti libqt5core5a
