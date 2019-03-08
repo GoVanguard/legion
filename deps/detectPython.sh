9794f85ab40a0248f058e5886e2a59dac22401a4
jchoy14 <28782996+jchoy14@users.noreply.github.com>
2019-03-08 14:52:51 -0500

    Fedora switched to Python 3.7

--- deps/detectPython.sh
+++ deps/detectPython.sh
@@ -3,7 +3,7 @@
 testForPython=`python --version 2>&1`
 testForPython2=`python3 --version 2>&1`
 testForPython3=`python3.6 --version 2>&1`
-testForPython37=`python3.7 --version 2>&1`
+testForPython4=`python3.7 --version 2>&1`
 releaseOutput=`cat /etc/os-release`
 
 if [[ ${releaseOutput} == *"Fedora"* ]]
@@ -12,7 +12,7 @@ if [[ ${releaseOutput} == *"Fedora"* ]]
 			pythonBin='python'
 		elif [[ $testForPython2 == *"3.7"* ]]; then
 			pythonBin='python3'
-		elif [[ $testForPython3 == *"3.7"* ]] && [[ $testForPython3 != *"not found"* ]]; then
+		elif [[ $testForPython4 == *"3.7"* ]] && [[ $testForPython4 != *"not found"* ]]; then
 			pythonBin='python3.7'
 		else
 			pythonBin='Missing'
@@ -34,18 +34,32 @@ fi
 testForPip=`pip --version 2>&1`
 testForPip2=`pip3 --version 2>&1`
 testForPip3=`pip3.6 --version 2>&1`
+testForPip4=`pip3.7 --version 2>&1`
 
-if [[ $testForPip == *"3.6"* ]]; then
-    pipBin='pip'
-elif [[ $testForPip2 == *"3.6"* ]]; then
-    pipBin='pip3'
-elif [[ $testForPip3 == *"3.6"* ]] && [[ $testForPip3 != *"not found"* ]]; then
-    pipBin='pip3.6'
+if [[ ${releaseOutput} == *"Fedora"* ]]
+	then
+		if [[ $testForPip == *"3.7"* ]]; then
+			pipBin='pip'
+		elif [[ $testForPip2 == *"3.7"* ]]; then
+			pipBin='pip3'
+		elif [[ $testForPip3 == *"3.7"* ]] && [[ $testForPip3 != *"not found"* ]]; then
+			pipBin='pip3.7'
+		else
+			pipBin='Missing'
+		fi
 else
-    pipBin='Missing'
+	if [[ $testForPip == *"3.6"* ]]; then
+		pipBin='pip'
+	elif [[ $testForPip2 == *"3.6"* ]]; then
+		pipBin='pip3'
+	elif [[ $testForPip3 == *"3.6"* ]] && [[ $testForPip3 != *"not found"* ]]; then
+		pipBin='pip3.6'
+	else
+		pipBin='Missing'
+	fi
 fi
 
-#echo "Pip 3.6 bin is ${pipBin} ($(which ${pipBin}))"
+#echo "Pip bin to use is ${pipBin} ($(which ${pipBin}))"
 
 export PYTHON3BIN=$(which ${pythonBin})
 export PIP3BIN=$(which ${pipBin})
