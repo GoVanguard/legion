023360184294959317548babe5fabde42478d534
GitHub <noreply@github.com>
2019-04-02 16:19:28 -0400

    Added git

--- deps/installDeps.sh
+++ deps/installDeps.sh
@@ -7,4 +7,4 @@ echo "Checking Apt..."
 runAptGetUpdate
 
 echo "Installing deps..."
-DEBIAN_FRONTEND="noninteractive" apt-get -yqqq --allow-unauthenticated --force-yes -o DPkg::Options::="--force-overwrite" -o DPkg::Options::="--force-confdef" install nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 python-impacket ruby perl dnsmap
+DEBIAN_FRONTEND="noninteractive" apt-get -yqqq --allow-unauthenticated --force-yes -o DPkg::Options::="--force-overwrite" -o DPkg::Options::="--force-confdef" install nmap finger hydra nikto whatweb nbtscan nfs-common rpcbind smbclient sra-toolkit git ldap-utils sslscan rwho medusa x11-apps cutycapt leafpad xvfb imagemagick eog hping3 python-impacket ruby perl dnsmap
