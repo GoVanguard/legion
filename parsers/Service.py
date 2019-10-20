d5dba21bdb4ca9a0855a6611ea0d8534a7107d6c
Dmitriy Dubson <d.dubson@gmail.com>
2019-10-20 12:48:18 -0700

    Move more parser examples into example directory

--- parsers/Service.py
+++ parsers/Service.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 from app.logging.legionLog import log
 
-__author__ =  'yunshu(wustyunshu@hotmail.com)'
-__version__=  '0.2'
+__author__ = 'yunshu(wustyunshu@hotmail.com)'
+__version__ = '0.2'
 __modified_by = 'ketchup'
 
-import sys
-import xml.dom.minidom
 
 class Service:
     extrainfo = ''
@@ -15,27 +13,9 @@ class Service:
     fingerprint = ''
     version = ''
 
-    def __init__( self, ServiceNode ):
+    def __init__(self, ServiceNode):
         self.extrainfo = ServiceNode.getAttribute('extrainfo')
         self.name = ServiceNode.getAttribute('name')
         self.product = ServiceNode.getAttribute('product')
         self.fingerprint = ServiceNode.getAttribute('servicefp')
         self.version = ServiceNode.getAttribute('version')
-
-
-if __name__ == '__main__':
-
-    dom = xml.dom.minidom.parse('i.xml')
-
-    service_nodes = dom.getElementsByTagName('service')
-    if len(service_nodes) == 0:
-        sys.exit()
-
-    node = dom.getElementsByTagName('service')[0]
-
-    s = Service( node )
-    log.info(s.name)
-    log.info(s.product)
-    log.info(s.version)
-    log.info(s.extrainfo)
-    log.info(s.fingerprint)
