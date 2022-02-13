0b5b435a1328ccbabad0dfc143c77c2fd0adb452
Dmitriy Dubson <d.dubson@gmail.com>
2022-02-13 11:02:50 -0800

    Fix dnsmap install for Ubuntu 18 and earlier
    references #211

--- deps/Ubuntu-16WSL.sh
+++ deps/Ubuntu-16WSL.sh
@@ -3,6 +3,12 @@
 chmod a+x ./deps/*.sh
 
 ./deps/installDeps.sh
+
+# Install dnsmap manually as Ubuntu versions earlier than 20.04 do not have dnsmap available in primary distro repositories.
+curl -q -O http://archive.ubuntu.com/ubuntu/pool/universe/d/dnsmap/dnsmap_0.35-3_amd64.deb -o dnsmap_0.35-3_amd64.deb && \
+  sudo dpkg -i dnsmap_0.35-3_amd64.deb && \
+  rm dnsmap_0.35-3_amd64.deb
+
 ./deps/installPython36.sh
 
 source ./deps/detectPython.sh
