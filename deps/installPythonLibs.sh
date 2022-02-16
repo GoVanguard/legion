771597685f97ddcd363ef7f297194704da6663f4
Dmitriy Dubson <d.dubson@gmail.com>
2022-02-16 15:38:58 -0800

    Move service-identity dependency to requirements.txt

--- deps/installPythonLibs.sh
+++ deps/installPythonLibs.sh
@@ -4,6 +4,5 @@ source ./deps/detectPython.sh
 
 # Setup Python deps
 ${PIP3BIN} install -r requirements.txt --upgrade
-${PIP3BIN} install service-identity --upgrade
 
 ${PYTHON3BIN} ./deps/primeExploitDb.py
